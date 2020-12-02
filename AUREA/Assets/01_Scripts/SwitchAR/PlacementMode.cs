11c4cba691dcae99fc19d8d1671ba0a5f805a5d0
Dennis <dennishawran@gmail.com>
2020-12-02 09:03:14 +0100

    Deleted everything

--- AUREA/Assets/01_Scripts/SwitchAR/PlacementMode.cs
+++
@@ -1,6 +0,0 @@
-public enum ARPlacementMode
-{
-    FREE_ASPEKT,
-    PLACE_ON_GROUND,
-    PLACE_IN_AIR
-}
--- AUREA/Assets/02_Data/AureaList.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "HeroList", menuName = "Data/HeroList")]
-public class AureaList : ScriptableObject
-{
-    public List<AureaData> aureas = new List<AureaData>();
-}
