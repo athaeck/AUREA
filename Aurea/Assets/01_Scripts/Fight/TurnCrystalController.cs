76971dd9f90bf37d4db76a17e97a085d2aa57142
Dennis <dennishawran@gmail.com>
2021-01-02 22:40:21 +0100

    Turn Visualizer

---
+++ Aurea/Assets/01_Scripts/Fight/TurnCrystalController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PositionController))]
+public class TurnCrystalController : MonoBehaviour
+{
+    [SerializeField]
+    private FightController controller = null;
+
+    [SerializeField]
+    private PositionController positionController = null;
+
+    [SerializeField]
+    private Transform playerPosition = null;
+
+    [SerializeField]
+    private Transform enemyPosition = null;
+
+    private void Start()
+    {
+        positionController = GetComponent<PositionController>();
+        controller.TurnChanged += TurnChanged;
+    }
+
+    private void TurnChanged(PlayerController _playerController)
+    {
+        if (_playerController.isPlayer)
+            positionController.TakeTarget(playerPosition);
+        else
+            positionController.TakeTarget(enemyPosition);
+    }
+}
