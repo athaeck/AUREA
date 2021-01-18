0958fc493cd58dae2d829870c670cb4b2b4cac28
Dennis <dennishawran@gmail.com>
2021-01-18 22:37:37 +0100

    Health Slider and TextMeshPro Import

--- Aurea/Assets/01_Scripts/Common/Billboard.cs
+++ Aurea/Assets/01_Scripts/Common/Billboard.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
-    [SerializeField]
-    private Transform cam = null;
+    // [SerializeField]
+    // private Transform cam = null;
 
-    [SerializeField]
-    private Transform ARcam = null;
+    // [SerializeField]
+    // private Transform ARcam = null;
 
     void FixedUpdate()
     {
-        if (Player.Instance.IsArOn())
-            transform.LookAt(transform.position + ARcam.forward);
-        else
-            transform.LookAt(transform.position + cam.forward);
+        transform.LookAt(transform.position + CameraController.Instance.activeCamera.transform.forward);
+        // if (Player.Instance.IsArOn())
+        //     transform.LookAt(transform.position + ARcam.forward);
+        // else
+        //     transform.LookAt(transform.position + cam.forward);
     }
 }
---
+++ Aurea/Assets/01_Scripts/Fight/HealthSlider.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSlider : MonoBehaviour
+{
+    public Sprite greenSprite = null;
+    public Sprite orangeSprite = null;
+    public Sprite redSprite = null;
+    public Aurea aurea = null;
+    public Slider fillSlider = null;
+    public Image fillImage = null;
+
+    private void Start()
+    {
+        if (!aurea)
+        {
+            Debug.Log("No Aurea");
+            return;
+        }
+
+        aurea.ChangedLifepoints += UpdateLife;
+    }
+
+    void UpdateLife()
+    {
+        float fillAmount = aurea.GetLifePointsLeft() / aurea.GetLifePointsMax();
+        fillSlider.value = fillAmount;
+
+        if (fillAmount > 0.6f)
+            fillImage.sprite = greenSprite;
+        else if (fillAmount > 0.3f)
+            fillImage.sprite = orangeSprite;
+        else
+            fillImage.sprite = redSprite;
+    }
+}
