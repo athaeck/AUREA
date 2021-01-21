4feb96fd574c8213f741608c35c5836207d1fee5
Dennis <dennishawran@gmail.com>
2021-01-21 11:25:07 +0100

    Improved movement

--- Aurea/Assets/01_Scripts/Common/MovementController.cs
+++ Aurea/Assets/01_Scripts/Common/MovementController.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody))]
 public class MovementController : MonoBehaviour
 {
-    CharacterController controller;
+    Animator anim = null;
+    Rigidbody rb = null;
     public Vector3 destination { get; set; }
 
     [SerializeField]
@@ -14,28 +17,60 @@ public class MovementController : MonoBehaviour
     private float rotationSpeed = 175;
 
     [SerializeField]
-    private float rotationThreshold = 1f;
+    private float threshold = 1f;
+
+    [SerializeField]
+    private float gravity = -9.81f;
+
+    [SerializeField]
+    private float raycastDistance = 1f;
 
 
     private void Awake()
     {
-        controller = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
         destination = transform.position;
     }
 
     private void FixedUpdate()
     {
-        Vector3 dir = (destination - transform.position).normalized * speed * Time.deltaTime;
-        controller.Move(dir);
-
-        if ((destination - transform.position).magnitude > rotationThreshold)
+        if ((destination - transform.position).magnitude > threshold)
         {
+            Vector3 dir = (destination - transform.position).normalized * speed * Time.deltaTime;
+
+            if (!isGrounded())
+                rb.AddForce(transform.up * gravity, ForceMode.Acceleration);
+
+            rb.MovePosition(transform.position + dir);
+
             Vector3 newRot = Vector3.Slerp(transform.position + transform.forward, destination, rotationSpeed * Time.deltaTime);
             transform.LookAt(newRot);
+
+            float animSpeed = Remap(dir.magnitude, 0, 0.1f, 0, 1);
+            anim.SetFloat("Speed", animSpeed);
+        } else {
+            destination = transform.position;
+            anim.SetFloat("Speed", 0);
         }
 
+        rb.angularVelocity = Vector3.zero;
+    }
 
+    bool isGrounded()
+    {
+        if(Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, raycastDistance)) {
+            if(hit.collider.CompareTag("Walkable")) {
+                return true;
+            }
+        }
+
+        return false;
     }
     public float GetSpeed() { return speed; }
 
+    float Remap(float value, float from1, float to1, float from2, float to2)
+    {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
 }
