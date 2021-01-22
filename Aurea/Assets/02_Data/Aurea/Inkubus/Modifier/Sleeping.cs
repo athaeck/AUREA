ec73e8f025cccd89b9fe83ad8750bb8ecbbd0a4a
Dennis <dennishawran@gmail.com>
2021-01-22 17:49:06 +0100

    Inkubus Schlafenszeit

---
+++ Aurea/Assets/02_Data/Aurea/Inkubus/Modifier/Sleeping.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Aurea))]
+public class Sleeping : MonoBehaviour
+{
+    int sleepRandomFor = 3;
+    Aurea aurea = null;
+
+    [SerializeField]
+    int roundsLeft = 0;
+
+    void Start()
+    {
+        aurea = GetComponent<Aurea>();
+        roundsLeft = UnityEngine.Random.Range(0, sleepRandomFor);
+        aurea.GetPlayer().SelectedAurea += BlockAurea;
+        aurea.GetPlayer().StartedTurn += NewTurn;
+    }
+
+    void BlockAurea(Aurea _aurea) {
+        if(aurea == _aurea) {
+            aurea.GetPlayer().Select(null);
+        }
+    }
+
+    void NewTurn() {
+        roundsLeft--;
+        if(roundsLeft <= 0) {
+            aurea.GetPlayer().SelectedAurea -= BlockAurea;
+            aurea.GetPlayer().StartedTurn -= NewTurn;
+            DestroyImmediate(this);
+        }
+    }
+}
