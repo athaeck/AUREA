24381843ca1aa637fb9eccc71cb99d172d515f87
Dennis Hawran <dennishawran@gmail.com>
2021-03-01 14:52:27 +0100

    Created Skill templates for Illusian

---
+++ Aurea/Assets/02_Data/Aurea/Illusian/Skills/Transfusion/Transfusion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Alptraum", menuName = "Skills/Illusian/Transfusion")]
+public class Transfusion : Skill
+{
+    public override bool IsTargetValid(Aurea _target, Aurea _sender) {
+        return true;
+    }
+
+    public override bool CheckTargets(List<Aurea> _targets, Aurea _sender) {
+        return true;
+    }
+
+    public override void Use(Damage _dmg) {
+
+    }
+}
