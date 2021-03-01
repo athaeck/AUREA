ad280110377f8976d41e70b692cb12fa0ad54359
Dennis Hawran <dennishawran@gmail.com>
2021-03-01 16:11:00 +0100

    Schattenwürgegriff

--- Aurea/Assets/02_Data/Aurea/Illusian/Skills/Schattenwürgegriff/Schattenwürgegriff.cs
+++ Aurea/Assets/02_Data/Aurea/Illusian/Skills/Schattenwürgegriff/Schattenwürgegriff.cs
@@ -5,15 +5,41 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Schattenwürgegriff", menuName = "Skills/Illusian/Schattenwürgegriff")]
 public class Schattenwürgegriff : Skill
 {
+    [SerializeField]
+    private float magicDamageMultiplier = 1f;
+
+    [SerializeField]
+    private float attackDelay = 2f;
+
+    [SerializeField]
+    private AttackAnimationController animation = null;
+
+    public override void Use(Damage _dmg) {
+        _dmg.physicalDamage *= magicDamageMultiplier;
+        _dmg.attackDelay = attackDelay;
+
+        if (Player.Instance.AnimationsOn() && animation)
+            animation.StartAnimation(_dmg);
+
+
+        foreach (Aurea target in _dmg.targets)
+        {
+            Damage dmg = _dmg.Copy();
+            target.TakeDamage(dmg);
+        }
+    }
     public override bool IsTargetValid(Aurea _target, Aurea _sender) {
+        if (_target.GetPlayer() == _sender.GetPlayer())
+            return false;
+
         return true;
     }
 
     public override bool CheckTargets(List<Aurea> _targets, Aurea _sender) {
-        return true;
+        if (_targets.Count > 0 && IsTargetValid(_targets[0], _sender))
+            return true;
+
+        return false;
     }
 
-    public override void Use(Damage _dmg) {
-
-    }
 }
