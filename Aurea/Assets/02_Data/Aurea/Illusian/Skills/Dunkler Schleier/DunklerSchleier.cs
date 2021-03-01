71542e61d5e2b4445315c1c678e26a8fb13bfb0f
Dennis Hawran <dennishawran@gmail.com>
2021-03-01 17:14:15 +0100

    Dunkler Schleier

--- Aurea/Assets/02_Data/Aurea/Illusian/Skills/Dunkler Schleier/DunklerSchleier.cs
+++ Aurea/Assets/02_Data/Aurea/Illusian/Skills/Dunkler Schleier/DunklerSchleier.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "DunklerSchleier", menuName = "Skills/Illusian/Dunkler Schleier")]
 public class DunklerSchleier : Skill
 {
-    public override bool IsTargetValid(Aurea _target, Aurea _sender) {
+    public override void Use(Damage _dmg)
+    {
+        foreach (string mod in modifier)
+        {
+            System.Type modifierScript = System.Type.GetType(mod);
+            Component component = _dmg.sender.GetComponent(modifierScript);
+
+            if (component)
+                component.SendMessage("Kill");
+
+            _dmg.sender.gameObject.AddComponent(modifierScript);
+        }
+
+    }
+    public override bool IsTargetValid(Aurea _target, Aurea _sender)
+    {
         return true;
     }
 
-    public override bool CheckTargets(List<Aurea> _targets, Aurea _sender) {
+    public override bool CheckTargets(List<Aurea> _targets, Aurea _sender)
+    {
         return true;
     }
 
-    public override void Use(Damage _dmg) {
-
-    }
 }
