d7728f2c6a8f2f752e5e66631967efbfb7a388b9
Dennis Hawran <dennishawran@gmail.com>
2021-03-02 07:54:54 +0100

    Created Dragon Skill Templates

---
+++ Aurea/Assets/02_Data/Aurea/Dragon/Skills/Feuerball/Feuerball.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Feuerball", menuName = "Skills/Dragon/Feuerball")]
+public class Feuerball : Skill
+{
+    [SerializeField]
+    private float magicDamageMultiplier = 1f;
+
+    public override void Use(Damage _dmg) {
+        _dmg.physicalDamage *= magicDamageMultiplier;
+        _dmg.attackDelay = attackDelay;
+
+        if (Player.Instance.AnimationsOn() && animation)
+            animation.StartAnimation(_dmg);
+
+        foreach (Aurea target in _dmg.targets)
+        {
+            Damage dmg = _dmg.Copy();
+            target.TakeDamage(dmg);
+        }
+    }
+    public override bool IsTargetValid(Aurea _target, Aurea _sender) {
+        if (_target.GetPlayer() == _sender.GetPlayer())
+            return false;
+
+        return true;
+    }
+
+    public override bool CheckTargets(List<Aurea> _targets, Aurea _sender) {
+        if (_targets.Count > 0 && IsTargetValid(_targets[0], _sender))
+            return true;
+
+        return false;
+    }
+}
