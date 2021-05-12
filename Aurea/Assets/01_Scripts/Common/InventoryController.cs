3eda1b43ec68b351a9c2e870116ae6b90d33ef35
NickHaecker <nick.philipp.haecker@hs-furtwangen.de>
2021-05-12 08:41:48 +0200

    fix

--- Aurea/Assets/01_Scripts/Common/InventoryController.cs
+++ Aurea/Assets/01_Scripts/Common/InventoryController.cs
@@ -21,9 +21,15 @@ public class InventoryController : MonoBehaviour
 
     private Text _title = null;
 
+    private bool clicked = false;
+
     public void InitInventory()
     {
-        SetItems();
+        if(!clicked)
+        {
+            SetItems();
+        }
+        clicked = true;
     }
     private void Start()
     {
@@ -33,41 +39,60 @@ public class InventoryController : MonoBehaviour
         if(inventoryHUD != null)
         {
             inventoryHUD.SetActive(false);
+            clicked = false;
+            List<GameObject> items = new List<GameObject>();
+            int length = _container.transform.childCount;
+            for(int i = 0 ; i < length ; i++)
+            {
+                items.Add(_container.transform.GetChild(i).transform.gameObject);
+            }
+            for(int i = length-1 ; i >= 0 ; i--)
+            {
+                Destroy(items[i]);
+            }
         }
     }
     private void Update()
     {
-       ownedItems = Player.Instance.GetItems();
+       //ownedItems = Player.Instance.GetItems();
     }
     private void SetItems()
     {
+        ownedItems = Player.Instance.GetItems();
         if(inventoryHUD != null)
         {
             inventoryHUD.SetActive(true);
         }
-        int i = 0;
+        int index = 0;
         foreach(PlayerItemData item in ownedItems)
         {
-            int x = 1;
-            int y = 0;
-            foreach(PlayerItemData it in ownedItems)
-            {
-                if(item.name.Equals(it.name))
-                {
-                    if(i != y)
-                    {
-                        x++;
-                    }
-                }
-            }
+            int quantity = CheckQuantity(item,index);
             GameObject iO = Instantiate(_itemObject,_container.transform.position,_container.transform.rotation,_container.transform);
-            InventoryItem iI = iO.AddComponent<InventoryItem>() as InventoryItem;
-            iI.SetItem(item.description,item.name,x,this);
-            i++;
+            InventoryItem iI = iO.AddComponent<InventoryItem>();
+            iI.SetItem(item.description,item.name,quantity,this);
+            index++;
         }
     }
     public void ChooseItem(string item)
     {
         Debug.Log(item);
+    }
+    private int CheckQuantity(PlayerItemData item, int currentIndex) {
+        int quantity = 1;
+        int index = 0;
+        foreach(PlayerItemData it in ownedItems)
+        {
+            if(currentIndex != index)
+            {
+                if(item.name == it.name)
+                {
+
+                    quantity++;
+
+                }
+            }
+            index++;
+        }
+        return quantity;
     }
 }
